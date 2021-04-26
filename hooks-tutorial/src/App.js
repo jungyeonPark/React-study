b597df75993747634d7c69071d50069e82318a94
jungyeonPark <kshpjy2626@gmail.com>
2021-04-27 00:45:42 +0900

    8장까지 공부 완료

---
+++ hooks-tutorial/src/App.js
@@ -0,0 +1,10 @@
+import { useState } from "react";
+import Average from "./Average";
+import Counter from "./Counter";
+import Info from "./Info";
+
+const App = () => {
+  return <Info />;
+};
+
+export default App;
