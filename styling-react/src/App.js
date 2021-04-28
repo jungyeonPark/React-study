426a1f434a6d019e24f16d76d6cd1cd95c573bb8
jungyeonPark <kshpjy2626@gmail.com>
2021-04-29 07:01:07 +0900

    10.2까지 완료

--- styling-react/src/App.js
+++ styling-react/src/App.js
@@ -1,12 +1,11 @@
 import React, { Component } from "react";
-import "./App.css";
-import SassComponent from "./SassComponent";
+import StyledComponent from "./StyledComponent";
 
 class App extends Component {
   render() {
     return (
       <div>
-        <SassComponent />
+        <StyledComponent />
       </div>
     );
   }
--- styling-react/src/SassComponent.js
+++ styling-react/src/SassComponent.js
@@ -1,20 +1,18 @@
 import React from "react";
 import "./SassComponent.scss";
 
-const SassComponent = () => {
-  return (
+const SassComponent = () => (
+  <div className="SassComponent">
     <div className="SassComponent">
-      <div className="SassComponent">
-        <div className="box red" />
-        <div className="box orange" />
-        <div className="box yellow" />
-        <div className="box green" />
-        <div className="box blue" />
-        <div className="box indigo" />
-        <div className="box violet" />
-      </div>
+      <div className="box red" />
+      <div className="box orange" />
+      <div className="box yellow" />
+      <div className="box green" />
+      <div className="box blue" />
+      <div className="box indigo" />
+      <div className="box violet" />
     </div>
-  );
-};
+  </div>
+);
 
 export default SassComponent;
